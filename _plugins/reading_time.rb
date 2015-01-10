0ee0e26b2f15b070e76c69088e47979b013e069d
Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
2015-01-11 00:38:04 +0100

    Setup blog/index.html and other blog related stuff.

---
+++ _plugins/reading_time.rb
@@ -0,0 +1,18 @@
+# Outputs the reading time
+
+# Read this in â€œabout 4 minutesâ€
+# Put into your _plugins dir in your Jekyll site
+# Usage: Read this in about {{ page.content | reading_time }}
+
+module ReadingTimeFilter
+  def reading_time( input )
+    words_per_minute = 180
+
+    words = input.split.size;
+    minutes = ( words / words_per_minute ).floor
+    minutes_label = minutes === 1 ? " Minute" : " Minuten"
+    minutes > 0 ? "etwa #{minutes} #{minutes_label}" : "weniger als 1 Minute"
+  end
+end
+
+Liquid::Template.register_filter(ReadingTimeFilter)
